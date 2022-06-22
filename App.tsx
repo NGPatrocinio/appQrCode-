fbe00501d6e4719f6074251d60a56c0a82e9c539
Nimay Patrocinio <nimaypatrocinio@gmail.com>
2022-06-22 02:02:47 +0100

    1qrCode

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, TouchableOpacity } from 'react-native';
+import { RNCamera, FaceDetector,  } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default function App() {
+
+  const onSuccess = (e: any) => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Qr Code</Text>
+      <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text>
+            Go to{' '}
+            <Text>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity>
+            <Text>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
       <StatusBar style="auto" />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Open up App.tsx to start working on your app!</Text>
+      </View>
     </View>
+
   );
 }
 
@@ -16,5 +45,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
+  footer: {
+    paddingLeft: "3rem",
+    backgroundColor: "lightblue",
+    position: 'absolute', 
+    bottom: 0,
+    width: "100%" ,
+    height: "2.5rem" ,  
+  },
+
+  footerText: {
+    lineHeight: 40,
+  }
 });
+
+expo install expo-barcode-scanner
